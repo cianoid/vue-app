b35531571ae9e5e439475c679f899a39deb0fe89
Igor Shatava <cianoid@yandex.ru>
2025-05-04 08:33:54 +0300

    add vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+let curDate = new Date();
+let dateString = curDate.toLocaleDateString();
+</script>
+
+<template>
+  <div id="app">
+    <h1>Vue basics</h1>
+    <p>Current date: {{ dateString }}</p>
+  </div>
+</template>
+
+<style scoped>
+</style>

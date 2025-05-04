9a49db3d77788dc9fef7b9fe9e02a5e6070e50ed
Igor <46224693+cianoid@users.noreply.github.com>
2025-05-04 08:38:05 +0300

    Merge pull request #1
    * add vue
    
    * review fix

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const dateString = ref(new Date().toLocaleDateString());
+</script>
+
+<template>
+  <div id="app">
+    <h1>Vue basics</h1>
+    <p>Current date: {{ dateString }}</p>
+  </div>
+</template>
+
+<style scoped>
+</style>

cc37c70e985867eb366d63ef2d317d0ab24e4ad3
Igor Shatava <cianoid@yandex.ru>
2025-05-08 21:22:32 +0300

    add card states

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import Button from "./components/Button.vue";
 import Score from "./components/Score.vue";
 import Card from "./components/Card.vue";
 
-function getTurned(flipped) {
-  console.log(`flipped = ${flipped}`);
+function getTurned() {
 }
 
 let score = ref(100);
-let cards = ref([{
-  word: "слово",
-  translation: "word",
-  state: "closed",
-  status: "pending",
-}])
+let cards = ref([
+  {
+    word: "word",
+    translation: "слово",
+    state: "closed",
+    status: "pending",
+  },
+  {
+    word: "elephant",
+    translation: "слон",
+    state: "opened",
+    status: "pending",
+  },
+  {
+    word: "car",
+    translation: "автомобиль",
+    state: "opened",
+    status: "success",
+  },
+  {
+    word: "cablecar",
+    translation: "канатная дорога",
+    state: "opened",
+    status: "fail",
+  },
+])
+
+let cardsData = computed(() => {
+  let data = {};
+  for (let i = 0; i < cards.value.length; i++) {
+    data[i] = cards.value[i];
+    data[i].number = i > 9 ? `${i+1}` : `0${i+1}`;
+  }
+  return data;
+})
 </script>
 
 <template>
@@ -29,7 +57,9 @@ let cards = ref([{
     </Button>
   </div>
 
-<Card @flipped="getTurned" v-bind="cards[0]"/>
+  <div class="cards">
+    <Card v-for="item in cardsData" :key="item.word" v-bind="item" @flipped="getTurned" />
+  </div>
 
 </template>
 
@@ -37,20 +67,27 @@ let cards = ref([{
 .main {
   background-color: var(--color-bg);
 }
-.menu{
-  height:121px;
-  width:100%;
+.cards{
+  display: flex;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 1rem;
+}
+
+.menu {
+  height: 121px;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
 
 }
-.title{
-font-weight: 700;
-font-size: 16px;
-line-height: 150%;
-letter-spacing: 0.12em;
-color: #222;
+
+.title {
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 150%;
+  letter-spacing: 0.12em;
+  color: #222;
   text-transform: uppercase;
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,21 @@
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import IconFail from "../icons/icon.fail.vue";
+import IconSuccess from "../icons/icon.success.vue";
 
-const {word = "-", translation = "-", state = "closed", status = "pending"} = defineProps({
+const {number = "00", word = "-", translation = "-", state = "closed", status = "pending"} = defineProps({
+  number: String,
   word: String,
   translation: String,
   state: {
-    type:String,
+    type: String,
     default: "closed",
     validator(value) {
       return value === "closed" || value === "opened"
     },
   },
   status: {
-    type:String,
+    type: String,
     default: "pending",
     validator(value) {
       return value === "pending" || value === "success" || value === "fail"
@@ -27,17 +30,38 @@ const flipped = ref(false);
 const turnCard = () => {
   flipped.value = !flipped.value;
   emit("flipped", flipped.value);
-  console.log(`turnCard ${flipped.value}`);
 };
+
+let isOpened = computed(() => state === 'opened');
+let isSuccess = computed(() => status === 'success');
+let isFail = computed(() => status === 'fail');
+let isPending = computed(() => status === 'pending');
 </script>
 
 <template>
   <div class="container">
     <div class="card">
-      <div class="card-number">06</div>
-      <div class="card-word">{{word}}</div>
-      <div class="card-translation">{{translation}}</div>
-      <div class="card-action" @click="turnCard()">Перевернуть</div>
+      <div class="card-top">
+        <div class="card-number">{{ number }}</div>
+
+        <div v-show="isOpened && isSuccess" class="card-result">
+          <IconSuccess/>
+        </div>
+        <div v-show="isOpened && isFail" class="card-result">
+          <IconFail/>
+        </div>
+      </div>
+
+      <div v-if="isOpened" class="card-word">{{ word }}</div>
+      <div v-else class="card-word">{{ translation }}</div>
+
+      <div v-show="!isOpened && isPending" class="card-action" @click="turnCard()">Перевернуть</div>
+      <div v-show="isOpened && (isSuccess || isFail)" class="card-action">Завершено</div>
+      <div v-show="isOpened && isPending" class="card-action">
+        <IconFail/>
+        <IconSuccess/>
+      </div>
+
     </div>
   </div>
 </template>
@@ -47,14 +71,11 @@ const turnCard = () => {
   border-radius: 16px;
   width: 250px;
   height: 376px;
-
   box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
   background: #fff;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
 }
 
 .card {
@@ -68,36 +89,57 @@ const turnCard = () => {
   justify-content: space-between;
 }
 
+.card-top {
+  display: flex;
+  align-items: center;
+  gap: 50px;
+}
+
 .card-number {
   width: 16px;
-  height: 16px;
+  height: 36px;
   background-color: #fff;
-  padding: 0 4px;
-
+  padding: 0 6px;
   font-weight: 400;
   font-size: 14px;
   text-align: center;
   color: #000;
+  position: relative;
+  top: -8px;
+  left: 10px;
+}
 
+.card-result {
+  width: 36px;
+  height: 36px;
+  background-color: #fff;
+  padding: 0 6px;
+  position: relative;
+  top: -18px;
 }
 
 .card-action {
-  padding: 0 4px;
+  padding: 0 6px;
   width: 97px;
   height: 18px;
   background-color: #fff;
   text-transform: uppercase;
-
   text-align: center;
-
   font-weight: 700;
   font-size: 12px;
   line-height: 150%;
   letter-spacing: 0.12em;
   color: #222;
   align-self: center;
-
   cursor: pointer;
+
+  position: relative;
+  bottom: -9px;
+
+  display: flex;
+  align-items: center;
+
+
 }
 
 .card-word {

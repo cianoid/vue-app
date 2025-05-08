108035098233aa524ee677dad3c42d9fd3b31846
Igor <46224693+cianoid@users.noreply.github.com>
2025-05-08 09:22:48 +0300

    3-events (#3)
    * add button
    
    * **Refactor structure and enhance button flexibility**
    
    Replaced `<main>` with `<div>` for better structural semantics and updated the Button component to support attribute bindings via `v-bind="$attrs"`. These changes improve flexibility and align with best practices for component design.
    
    * mr fix
    
    * mr fix
    
    * mr fix
    
    * add card and score
    
    * mr fix
    
    * mr fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
+<script setup>
+import Button from "./components/Button.vue";
+import Score from "./components/Score.vue";
+import Card from "./components/Card.vue";
+
+function getTurned(flipped) {
+  console.log(`flipped = ${flipped}`);
+}
+</script>
+
 <template>
   <div id="app" class="main">
+    <div class="menu">
+      <div class="title">Запомни слово</div>
+      <Score :score="38"/>
+    </div>
+
+
     <Button class="button-game-start">
       Начать игру
     </Button>
   </div>
+
+<Card @flipped="getTurned"/>
+
 </template>
-
-
-<script setup>
-import Button from "./components/Button.vue";
-</script>
 
 <style scoped>
 .main {
   background-color: var(--color-bg);
+}
+.menu{
+  height:121px;
+  width:100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+.title{
+font-weight: 700;
+font-size: 16px;
+line-height: 150%;
+letter-spacing: 0.12em;
+color: #222;
+  text-transform: uppercase;
 }
 </style>
